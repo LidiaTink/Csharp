a23c0e260d54e6ad643e2086335e6bbc8ae44232
Lidia <cola237@mail.ru>
2023-08-13 00:59:22 +1100

    Task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -11,11 +11,21 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
+{
     if (number1 > number3)
     {
-    int result = number1;
+    Console.WriteLine("Максимальное число "+number1);
     } 
+    else if (number2 > number3)
+    {
+        Console.WriteLine("Максимальное число "+number2);
+    }
+}
+else if (number2 > number3)
+    {
+        Console.WriteLine("Максимальное число "+number2);
+    }
     else
     {
-        int result = number2;
+    Console.WriteLine("Максимальное число "+number3);
     }
