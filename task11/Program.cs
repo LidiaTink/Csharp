20610f4e8c5ea9e5956a6782fa6d21081cdcac67
Lidia <cola237@mail.ru>
2023-08-13 16:43:16 +1100

    Task11

---
+++ task11/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цыфру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int number1 = number / 100;
+Console.WriteLine(number1);
+
+int number2 = number / 10 % 10;
+Console.WriteLine(number2);
+
+int number3 = number % 10;
+Console.WriteLine(number3);
+
+Console.WriteLine($"Итоговое число {number1}{number3}");
