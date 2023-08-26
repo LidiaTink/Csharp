32ae4fa11682fe96fa96cf21548f6e37c74186af
Lidia <cola237@mail.ru>
2023-08-26 14:46:00 +1100

    Task18

---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая по заданному номеру четверти показывает
+диапозон возможных координат точек в этой четверти 
+*/
+
+Console.WriteLine("Введите номер четверти:");
+int numberCH = Convert.ToInt32(Console.ReadLine());
+if (numberCH == 1)
+{
+    Console.WriteLine($"Все X > 0, все Y > 0");
+}
+else if (numberCH == 2)
+{
+    Console.WriteLine($"Все X < 0, все Y > 0");
+}
+    else if (numberCH == 3)
+    {
+        Console.WriteLine($"Все X < 0, все Y < 0");
+    }
+        else if (numberCH == 4)
+    {
+        Console.WriteLine($"Все X > 0, все Y < 0");
+    }
+        else
+        {
+            Console.WriteLine($"Такой четверти не существует");
+        }
