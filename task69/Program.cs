b0a9f7b7378fd37e128cb996b4cb3c8fbe918cbc
Lidia <cola237@mail.ru>
2023-09-19 07:10:02 +1100

    Task69

--- task69/Program.cs
+++ task69/Program.cs
@@ -2,13 +2,13 @@
 Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 */
 
-int result = ProdNumber(5,2);
+int result = ProdNumber(5, 2);
 
-int ProdNumber (int n, int m)
+int ProdNumber(int n, int m)
 {
-if (m==1)
-return n;
-else
-return n*ProdNumber(n,m-1);
+    if (m == 1)
+        return n;
+    else
+        return n * ProdNumber(n, m - 1);
 }
 System.Console.WriteLine(result);
